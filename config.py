05a328a26ff66356d2bd62cd3b16aa2020e0d584
marc-la <23857377@student.uwa.edu.au>
2025-06-13 12:40:12 +0800

    Configured .env with necessary env variables

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS', 'False') == 'True'
+    DEBUG = os.environ.get('DEBUG', 'False') == 'True'
